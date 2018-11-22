bb016f8304ece60fcda3f8f62e7026717b43cc81
sukun bao <345232213@qq.com>
2018-11-23 00:19:14 +0800

    dog and cat queue

---
+++ src/TEST/DogAndCatQueueTest.java
@@ -0,0 +1,84 @@
+package TEST;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import dog_and_cat_Queue.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zjbao on 2018/11/22.
+ */
+public class DogAndCatQueueTest {
+    @Test
+    public void add() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+
+    }
+
+    @Test
+    public void pollall() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+        System.out.println(queue.pollall());
+        System.out.println(queue.pollall());
+    }
+
+    @Test
+    public void pollDog() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+        System.out.println(queue.pollDog());
+    }
+
+    @Test
+    public void pollCat() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+        System.out.println(queue.pollCat());
+    }
+
+    @Test
+    public void isempty() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+        System.out.println(queue.isempty());
+    }
+
+    @Test
+    public void isDogempty() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+        System.out.println(queue.isDogempty());
+    }
+
+    @Test
+    public void isCatempty() throws Exception {
+        Cat cat =new Cat();
+        Dog dog =new Dog();
+        DogAndCatQueue queue = new DogAndCatQueue();
+        queue.add(cat);
+        queue.add(dog);
+        System.out.println(queue.isCatempty());
+    }
+
+}
---
+++ src/dog_and_cat_Queue/Dog.java
@@ -0,0 +1,10 @@
+package dog_and_cat_Queue;
+
+/**
+ * Created by zjbao on 2018/11/22.
+ */
+public class Dog extends Pet{
+    public Dog() {
+        super("dog");
+    }
+}
