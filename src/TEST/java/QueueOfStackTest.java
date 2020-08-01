6f52c351f0df09d101f7792c94ed875091a2462f
baosk <345232213@qq.com>
2020-08-01 22:21:39 +0800

    add QueueOfStack

---
+++ src/TEST/java/QueueOfStackTest.java
@@ -0,0 +1,22 @@
+import QueueOfStack.QueueOfStack;
+import org.junit.Test;
+
+/**
+ * @author zjbao123
+ * @version 1.0
+ * @since 2020-08-01 22:14:02
+ */
+public class QueueOfStackTest {
+    @Test
+    public void test(){
+        QueueOfStack myQueue = new QueueOfStack();
+        myQueue.add(1);
+        myQueue.add(2);
+        assert(myQueue.peek() == 1);
+        assert (myQueue.poll() == 1);
+        myQueue.add(3);
+        assert (myQueue.poll() == 2);
+        assert(myQueue.peek() == 3);
+        assert (myQueue.poll() == 3);
+    }
+}
